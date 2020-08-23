48dd4b886f22e5f8e813c7809cf19e5634ed74e2
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2020-08-23 21:03:37 +0530

    Updates

---
+++ precache-manifest.ef374b58440bcf2a0d0a422d4070d28c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a67ecb4506b79aef00affef7b4014dc",
+    "url": "/google/index.html"
+  },
+  {
+    "revision": "ec4a6bada9a26d2b52f6",
+    "url": "/google/static/css/main.cf08dc63.chunk.css"
+  },
+  {
+    "revision": "00e12433ebfe999247b9",
+    "url": "/google/static/js/2.32057de0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/google/static/js/2.32057de0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4a6bada9a26d2b52f6",
+    "url": "/google/static/js/main.bbf138df.chunk.js"
+  },
+  {
+    "revision": "1b26cbc1ed010002b959",
+    "url": "/google/static/js/runtime-main.5367d2cd.js"
+  }
+]);
